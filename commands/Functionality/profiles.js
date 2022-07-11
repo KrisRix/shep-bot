9dd0c24bff3c92f4bc6a95f3196160491562d626
KrisRix <66392473+KrisRix@users.noreply.github.com>
2022-07-11 09:32:59 -0400

    Profile skeleton pronouns test

--- commands/Functionality/profiles.js
+++ commands/Functionality/profiles.js
@@ -29,7 +29,7 @@ module.exports = {
 		// Find pronouns role
 		let memberPronouns;
 		for (const pronoun in pronounsList) {
-			if (interaction.member.roles.cache.has(pronoun)) {
+			if (interaction.member.roles.cache.has(pronoun.id)) {
 				memberPronouns = pronoun;
 			}
 			else {
